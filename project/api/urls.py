ab55436abcd3c2d7e209a74bf8572749ff1cb52a
Rahul Lokhande <rlokhande06244@gmail.com>
2024-11-08 10:51:27 +0530

    API

--- project/api/urls.py
+++ project/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("stu_info/<int:pk>",student_detail,name='name'),
-    path("stu_list/",student_list,name='student_list'),
+    # path("stu_info/<int:pk>",student_detail,name='name'),
+    # path("stu_list/",student_list,name='student_list'),
+    path("stu_api/",stu_api,name='stu_api')
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -7,35 +7,51 @@ from rest_framework.renderers import JSONRenderer
 
 # Create your views here.
 
-def student_list(req):
-    stu = Student.objects.all() # -----------------
-    print(type(stu))
-    print("Stu= ",stu)
-    print("stu.values()= ",stu.values())
-    print("stu.values_list()=",stu.values_list())
-    print("stu.values_list(col1,col2,col3)=",stu.values_list('name','roll','city'))
-    serializer = StudentSerializer(stu,many=True) # -------------------
-    print("Serializer= ",serializer)
-    print(serializer.data)
-    # json_data = JSONRenderer().render(serializer.data)
-    # print("Json_data = ",json_data)
-    # return HttpResponse(json_data,content_type='application/json')
-    # when we send json data from views then contet type must be a "application/json" 
-    return JsonResponse(serializer.data,safe=False) # -----------
-    # first argument of JsonResponse should be a dict, otherwise set safe=False
+# def student_list(req):
+#     stu = Student.objects.all() # -----------------
+    
+#     serializer = StudentSerializer(stu,many=True) # -------------------
+    
+#     # json_data = JSONRenderer().render(serializer.data)
+#     # print("Json_data = ",json_data)
+#     # return HttpResponse(json_data,content_type='application/json')
+#     # when we send json data from views then contet type must be a "application/json" 
+#     return JsonResponse(serializer.data,safe=False) # -----------
+#     # first argument of JsonResponse should be a dict, otherwise set safe=False
 
-def student_detail(req,pk):
-    user = Student.objects.get(pk=pk)
-    print(type(user))
-    print("Stu_name= ",user.name)
-    print("Stu_roll= ",user.roll)
-    print("Stu_city= ",user.city)
-    serializer = StudentSerializer(user)
-    print("Serializer= ",serializer)
-    print(serializer.data)
-    # json_data = JSONRenderer().render(serializer.data)
-    # print("Json_data = ",json_data)
-    # return HttpResponse(json_data,content_type='application/json')
-    # when we send json data from views then contet type must be a "application/json" 
-    return JsonResponse(serializer.data,safe=False)
-    # first argument of JsonResponse should be a dict, otherwise set safe=False
+# def student_detail(req,pk):
+#     user = Student.objects.get(pk=pk)
+#     # print(type(user))
+#     # print("Stu_name= ",user.name)
+#     # print("Stu_roll= ",user.roll)
+#     # print("Stu_city= ",user.city)
+#     serializer = StudentSerializer(user)
+#     # print("Serializer= ",serializer)
+#     # print(serializer.data)
+
+#     # json_data = JSONRenderer().render(serializer.data)
+#     # print("Json_data = ",json_data)
+#     # return HttpResponse(json_data,content_type='application/json')
+#     # when we send json data from views then contet type must be a "application/json" 
+#     return JsonResponse(serializer.data,safe=False)
+#     # first argument of JsonResponse should be a dict, otherwise set safe=False
+
+
+def stu_api(request):
+    if request.method=="GET":
+        json_data=request.body
+        print(json_data)
+        if json_data:
+            stream = io.BytesIO(json_data)
+            python_data=JSONPersar().parse(stream)
+            id = python_data.get('id')
+            stu=Student.objects.get(id=id)
+            serializer=StudentSerializer(stu)
+            return JsonResponse(serializer.data,safe=False)
+
+        else:
+            stu_all=Student.objects.all()
+            serializer=StudentSerializer(stu_all,many=True)
+            return JsonResponse(serializer.data,safe=False)
+
+        

2b668671da65461e11f2c0de48df444daf7f254d
Rahul Lokhande <rlokhande06244@gmail.com>
2024-11-11 14:54:04 +0530

    api

--- project/api/views.py
+++ project/api/views.py
@@ -255,6 +255,12 @@ from rest_framework.views import APIView
 #     def get(self, request, *args, **kwargs):
 #         return self.retrieve(request, *args, **kwargs)
 
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+
 
 
 

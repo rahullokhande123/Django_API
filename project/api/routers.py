a93c239d8cba9d44530675965c1631c6491f9bf0
Rahul Lokhande <rlokhande06244@gmail.com>
2024-11-18 23:07:50 +0530

    API

--- project/api/routers.py
+++ project/api/routers.py
@@ -4,6 +4,6 @@ from .views import RDStuData
 
 router=routers.DefaultRouter()
 router.register(r'movie',MovieViewSet,basename='movie')
-router.register(r'',RDStuData)
+router.register(r'student',RDStuData)
 
 urlpatterns=router.urls 
